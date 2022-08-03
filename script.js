74fc18734eb66b6d77eaf25d9b6e5f1648422d1f
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-03 13:12:38 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 
+//window.onload = function() {}
 
+function createGridLine(numPixels){
+  let grid = document.querySelector("#pixel-board"); 
+  for (let index = 1; index<= numPixels; index++) { 
+    let divLines = document.createElement("div");
+    //divLines.className = "lines";
+    //let ID = "id"+index;
+    //divLines.id = ID;
+      for (let contador = 1; contador <= numPixels; contador++){
+        let box = document.createElement("div");
+        box.className = "pixel";
+        divLines.appendChild(box);
+      }
+    grid.appendChild(divLines);
+   }
 
-
+}
+createGridLine(5);
 
