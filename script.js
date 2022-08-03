9461a04603455685a322b065f87fae8705a59ea1
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-03 18:40:59 -0300

    submissão do requisito 7

--- script.js
+++ script.js
@@ -21,8 +21,26 @@ createGridLine(5);
 
 function selectColor(corSelecionada){
   selecao = document.getElementById(corSelecionada);
-  selecao.classList.add("select");
+  selecao.classList.add("selected");
+}
+selectColor("corPreta");
+
+let pegaPalette = document.querySelector("#color-palette");
+let pegaCores = document.querySelectorAll(".color");
+
+function trocaClassSelected(evento) {
+  for (let index = 0; index < pegaCores.length; index ++){ //passando pelas divs da paleta e verificando qual tem a classe select
+  if (pegaCores[index].className === "color selected"){ //se a classe for color selected, remover o select
+     pegaCores[index].classList.remove("selected");
+      }
+  }
+//agora a gente precisa add o select na div que foi clicada (o alvo):
+  evento.target.classList.add("selected");
+}
+//por fim,precisamos fazer a função da troca de classe disparar quando houver clique em alguma das caixinhas da paleta
+for (let index = 0; index < pegaCores.length; index ++){
+    pegaCores[index].addEventListener("click", trocaClassSelected);
 }
 
-selectColor("corPreta");
+
 
