bbe5e7d09d1cd4645996a6562b476f0d20f535f7
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-04 11:27:11 -0300

    questão 8 FINALMENTE

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function createGridLine(numPixels){
 }
 createGridLine(5);
 
+//Requisito 7
 function selectColor(corSelecionada){
   selecao = document.getElementById(corSelecionada);
   selecao.classList.add("selected");
@@ -28,19 +29,36 @@ selectColor("corPreta");
 let pegaPalette = document.querySelector("#color-palette");
 let pegaCores = document.querySelectorAll(".color");
 
-function trocaClassSelected(evento) {
+function trocaClassSelected(event) {
   for (let index = 0; index < pegaCores.length; index ++){ //passando pelas divs da paleta e verificando qual tem a classe select
   if (pegaCores[index].className === "color selected"){ //se a classe for color selected, remover o select
      pegaCores[index].classList.remove("selected");
       }
   }
 //agora a gente precisa add o select na div que foi clicada (o alvo):
-  evento.target.classList.add("selected");
+  event.target.classList.add("selected");
 }
 //por fim,precisamos fazer a função da troca de classe disparar quando houver clique em alguma das caixinhas da paleta
 for (let index = 0; index < pegaCores.length; index ++){
     pegaCores[index].addEventListener("click", trocaClassSelected);
 }
+
+//requisito 8
+//O pixel do quadro clicado deve ter sua cor alterada para a cor selecionada na paleta de cores (exercício anterior)
+//Vamos pegar a cor do background da cor selecionada e armazenar em uma nova variável
+// function colorePixel(){;
+let pixelsAColorir = document.querySelector("#pixel-board"); //Pega a div do pixelboard
+
+function colorePixel(event) {  //função para estabelecer o que acontece quando o evento é acionado
+  let divSelected = document.querySelector(".selected"); //pega a div com a classe selected;
+  let corSelecionada = window.getComputedStyle(divSelected).backgroundColor; //pega a cor do background
+
+  let quadroDePixels = event.target;
+  quadroDePixels.style.backgroundColor = corSelecionada;
+  }
+ 
+ pixelsAColorir.addEventListener('click', colorePixel); //estabelece que, ao clicar, a função de colorir pixels é chamada
+ 
 
 
 
